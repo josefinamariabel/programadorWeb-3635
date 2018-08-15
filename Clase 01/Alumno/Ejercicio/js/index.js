e18d60e01b637c4ea9215e6d2a3e2658ff2d02d1
josefinamariabel <josefinamariabel@gmail.com>
2018-08-14 21:35:30 -0300

    ejercicio

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,43 @@
+console.log("hola")
+//ej 1
+
+//var fistName = prompt("Ingresa tu nombre")
+//console.log(fistName)
+
+//ej 2 
+
+// var edad = prompt ("escribi tu edad")
+
+// console.log (numero)
+
+// var parsedAge = parseFloat(numero, 10)
+
+// console.log(parsedAge + 10);
+
+//ej 3
+
+//  var numero = prompt ("escribi el nuemero de tu casa")
+
+//  console.log (numero)
+
+//  var parsedAge = parseFloat(numero, 10)
+
+//  var cp = 14
+
+//  var result = parsedAge - cp;
+
+//  console.log(result);
+
+
+var n1 = prompt (" nuemero ")
+var n2 = prompt (" nuemero ")
+
+var parsedAge = parseFloat(n1, 10)
+var parsedA = parseFloat(n2, 10)
+
+var result = (parsedAge + parsedA) 
+
+console.log(result);
+
+
+
